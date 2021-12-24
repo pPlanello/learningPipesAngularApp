1eec1f548b2cb2de914d0f3ba395df4b17783d42
pPlanello <planello96@gmail.com>
2021-12-24 13:11:40 +0100

    Creating PrimeNg Module and styles

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+// PrimeNg
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    ButtonModule,
+    CardModule
+  ],
+  imports: []
+})
+export class PrimeNgModule { }
